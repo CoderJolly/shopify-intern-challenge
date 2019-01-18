a92da71c7d89b8a1a12ac3ed529cbf92f2c5d356
Akshit Ahluwalia <akshitahluwalia@gmail.com>
2019-01-19 03:22:59 +0530

    apipie-rails gem installed for API documentation.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,13 +1,7 @@
 class Rack::Attack
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new 
 
-  throttle('api/ip', limit: 3, period: 10) do |req|
-    req.ip if req.subdomain == 'api'
-  end
-
-  class Request < ::Rack::Request
-    def subdomain
-      host.split('.').first
-    end
+  throttle('api/ip', limit: 8, period: 10) do |req|
+    req.ip
   end
 end
